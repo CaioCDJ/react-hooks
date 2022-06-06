d85eaf91b83eae72496ffdca04d3b4062cef73f5
CaioCDJ <caiodjesus1@hotmail.com>
2022-06-06 13:52:24 -0300

    useState

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route,Routes } from "react-router-dom";
+
+import Home from "../../views/examples/Home";
+import UseCallback from "../../views/examples/UseCallback";
+import UseContext from "../../views/examples/UseContext";
+import UseEffect from "../../views/examples/UseEffect";
+import UseRef from "../../views/examples/UseRef";
+import UseState from "../../views/examples/UseState";
+import UseMemo from "../../views/examples/UseMemo";
+import UseReducer from "../../views/examples/UseReducer";
+import NotFound from '../../views/examples/NotFound';
+import useCustom from '../../views/examples/UseCustom';
+
+export default ()=>{
+
+    return (
+        <main className="Content">
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/useState' element={<UseState />} />
+                <Route path='/useEffect' element={<UseEffect />} />
+                <Route path='/useRef' element={<UseRef />} />
+                <Route path='/useCallback' element={<UseCallback />} />
+                <Route path='/useContext' element={<UseContext />} />
+                <Route path='/useMemo' element={<UseMemo />} />
+                <Route path='/useReducer' element={<UseReducer />} />
+                <Route path='/useCustom' element={<useCustom />} />
+                <Route path='*' element={<NotFound />} />
+                
+            </Routes>
+        </main>
+    )
+}
---
+++ src/views/examples/UseCustom.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+
+export default  (props) => {
+    return (
+        <div className="UseCustom">
+            <PageTitle
+                title="Seu Hook"
+                subtitle="Vamos aprender como criar o nosso próprio Hook!"
+            />
+        </div>
+    )
+}
